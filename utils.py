75de71a577cc5d8d6b1d2c0cc4453c0b8402ac2d
James Booth <jabooth@gmail.com>
2016-06-16 21:51:34 +0100

    unified project listing in utils

--- utils.py
+++ utils.py
@@ -2,41 +2,64 @@ import os
 import subprocess
 from subprocess import CalledProcessError
 from functools import partial
+from collections import namedtuple
 
-# all projects using condaci
-PROJECTS = [
-            'conda-boost',
-            'conda-cherrypy',
-            'conda-dlib',
-            'conda-eigen',
-            'conda-enum',
-            'conda-ffmpeg',
-            'conda-flann',
-            'conda-freeimage',
-            'conda-imageio',
-            'conda-joblib',
-            'conda-mayavi',
-            'conda-menpo-pyvrml97',
-            'conda-opencv',
-            'conda-opencv3',
-            'conda-pathlib',
-            'conda-vlfeat',
-            'conda-vtk',
-            'cyassimp',
-            'cyffld2',
-            'cypico',
-            'cyrasterize',
-            'cyvlfeat',
-            'landmarkerio-server',
-            'menpo',
-            'menpo3d',
-            'menpobench',
-            'menpocli',
-            'menpodetect',
-            'menpofit',
-            'menpowidgets',
-            'vrml97'
-            ]
+Project = namedtuple('Project', ['name', 'versions'])
+
+# all projects using condaci along with the Python version they
+# need. Note that for non-python projects we can choose any single
+# python version.
+PROJECTS = [Project(*x) for x in
+            [('menpo', (2, 34, 35)),
+             ('menpodetect', (2, 34, 35)),
+             ('menpofit', (2, 34, 35)),
+             ('menpo3d', (2, 34, 35)),
+             ('menpocli', (2, 34, 35)),
+             ('menpowidgets', (2, 34, 35)),
+             
+             ('landmarkerio-server', (2,)),
+             ('menpobench', (2,)),
+
+
+             ('cyassimp', (2, 34, 35)),
+             ('cyrasterize', (2, 34, 35)),
+             ('cyvlfeat', (2, 34, 35)),
+             ('cyffld2', (2, 34, 35)),
+             ('cypico', (2, 34, 35)),
+             
+             ('conda-boost', (2, 34, 35)),
+             ('conda-cherrypy', (2, 34, 35)),
+             ('conda-dlib', (2, 34, 35)),
+             ('conda-opencv3',  (2, 34, 35)),
+             ('conda-ffmpeg', (2, 34, 35)),
+             ('conda-freeimage', (2, 34, 35)),
+             ('conda-imageio', (2, 34, 35)),
+             ('conda-joblib', (2, 34, 35)),
+             
+             # Python 2 only
+             ('conda-menpo-pyvrml97', (2,)),
+             ('conda-pathlib', (2,)),
+             
+             ('conda-vtk', (2, 34, 35)),
+             ('conda-traits', (2, 34, 35)),
+             ('conda-envisage', (2, 34, 35)),
+             ('conda-pyface', (2, 34, 35)),
+             ('conda-apptools', (2, 34, 35)),
+             ('conda-traitsui', (2, 34, 35)),
+             ('conda-mayavi', (2, 34, 35)),
+             
+             # Non-Python projects
+             ('vrml97', (2,)),
+             ('conda-flann', (2,)),
+             ('conda-eigen', (2,)),
+             ('conda-enum',  (2,)),
+             ('conda-vlfeat',  (35,)),
+
+             ('conda-opencv',  (35,))
+            
+            ]]
+
+PROJECT_NAMES = [p.name for p in PROJECTS]
 
 
 def load_file(fpath):
@@ -64,7 +87,7 @@ def clone_all_repos(working_dir):
     if not os.path.isdir(working_dir):
         print('creating path at {}'.format(working_dir))
         os.mkdir(working_dir)
-    for project in PROJECTS:
+    for project in PROJECT_NAMES:
         os.chdir(working_dir)
         try:
             clone_repo(project)
@@ -75,7 +98,7 @@ def clone_all_repos(working_dir):
 def apply_to_all_projects(working_dir, f, clone=True):
     if clone:
         clone_all_repos(working_dir)
-    for project in PROJECTS:
+    for project in PROJECT_NAMES:
         repo_dir = os.path.join(working_dir, project)
         print('processing {}...'.format(project))
         f(repo_dir)
